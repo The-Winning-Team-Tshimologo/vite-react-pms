eade28b20c2fea7ca289100a2b56c2a591344083
Tebogo <50196004+TMahlohela@users.noreply.github.com>
2024-03-01 12:19:39 +0200

    Add files via upload

---
+++ src/Dashboard/Dashboard.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from './Header'
+import Footer from './Footer'
+import Searchbar from './Searchbar'
+
+const Dashboard = () => {
+  return (
+    <>
+      <h2>Dash</h2>
+    </>
+  )
+}
+
+export default Dashboard
---
+++ src/NavSideBar/NavSideBar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NavSideBar = () => {
+  return (
+    <div>
+      <h2>NavSide</h2>
+    </div>
+  )
+}
+
+export default NavSideBar
