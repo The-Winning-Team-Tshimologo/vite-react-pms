04eae9940b05a5a453f612d74fb6eebdc31413fc
ismaelmours <monnapuleismaelmours@gmail.com>
2024-03-04 15:44:01 +0200

    I did parts of my work - I will continue tomorrow or later

--- src/pages/landingPage/LandingPage.jsx
+++ src/pages/landingPage/LandingPage.jsx
@@ -2,12 +2,17 @@ import Footer from '@/components/footer/Footer'
 import Header from '@/components/header/Header'
 import Hero from '@/components/hero/Hero'
 import React from 'react'
+import Service from "@/components/service/Service.jsx";
+import Testimonial from "@/components/testimony/Testimonial.jsx";
+import MyComponent from "@/components/testimony/MyComponent.jsx";
 
 const LandingPage = () => {
   return (
    <>
   <Header/>
   <Hero/>
+     <Service/>
+       <Testimonial/>
   <Footer/>
    </>
   )
