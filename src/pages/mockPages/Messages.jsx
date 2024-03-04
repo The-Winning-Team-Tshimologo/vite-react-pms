2b83f8bc50d564c70de3912f87161bf20bbf2ea0
Siyabonga Hadebe <92498693+Psyfo-C-YA@users.noreply.github.com>
2024-03-04 13:23:28 +0200

    Integrated Components
    Integrated available components to give the web app a seem less flow between pages. Set the foundation for authentication using context. Reconfigured tailwind.config.js cause it was altered and this broke our dependencies. Made the Sidebar and header as well as the collapse functionality it has an routing to other pages and keeping them in the position of outlet.

--- src/pages/mockPages/Messages.jsx
+++ src/pages/mockPages/Messages.jsx
@@ -1,16 +1,16 @@
 import Header from "@/components/header/Header";
 import React from "react";
 
-const Dashboard = () => {
+const Messages = () => {
   return (
     <>
       <Header />
       <div className="mt-10 mr-2 mx-2">
-        <h1>Dashboard</h1>
+        <h1>Mock Page 3</h1>
       </div>
-      <h2 className="mr-2 mx-2">Browse Professionals</h2>
+      <h2 className="mr-2 mx-2">Some useful info</h2>
     </>
   );
 };
 
-export default Dashboard;
+export default Messages;
