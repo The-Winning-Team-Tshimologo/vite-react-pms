3ebd9afa04e458cf79c6e9def9df992b74ff0871
Siyabonga Hadebe <92498693+Psyfo-C-YA@users.noreply.github.com>
2024-03-05 13:46:26 +0200

    Updating my branch (#16)
    * Update SignUp.css
    
    updated the background color for the input
    
    * i have added all of my required features
    
    * I did parts of my work - I will continue tomorrow or later
    
    * I did parts of my work - I will continue tomorrow or later
    
    * Revert "Experimenting fixing conflicts"
    
    * Add files via upload
    
    I added all features for landing page
    
    * Revert "Landing page"
    
    * Add files via upload
    
    i added all the features for landing page
    
    ---------
    
    Co-authored-by: Ismael Mours <83817080+IsmaelMours@users.noreply.github.com>
    Co-authored-by: Siya <89587223+siyabongaf018@users.noreply.github.com>
    Co-authored-by: Nompilo Zondo <nompilozondo44@gmail.com>
    Co-authored-by: ismaelmours <monnapuleismaelmours@gmail.com>

---
+++ src/components/testimony/Testimonial.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import Vector9 from '../../assets/Vector9.svg';
+import CircleImage from '../../assets/Ellipse23.png'; // Replace with your circular image path
+import './Testimonial.css'; // Import your CSS file for styling
+
+const Testimonial = () => {
+    return (
+        <div className="landing-page-inner">
+            <div className="testimonials-wrapper">
+                <h1 className="testimonials">Testimonials</h1>
+            </div>
+            <div className="frame-parent">
+                <div className="frame-group">
+                    <TestimonialCard />
+                    <TestimonialCard />
+                    <TestimonialCard />
+
+                </div>
+            </div>
+        </div>
+    );
+};
+
+const TestimonialCard = () => {
+    return (
+        <div className="vector-parent">
+            <div className="frame-child">
+                <img className="vector-image" loading="lazy" alt="" src={Vector9} />
+                <div className="super-cool-app-container">
+                    <p className="super-cool-app">Super Cool App</p>
+                    <p className="super-cool-app-description">
+                        Very convenient and easy to use. I love it!
+                    </p>
+                </div>
+                <CircleImageComponent />
+            </div>
+        </div>
+    );
+};
+
+const CircleImageComponent = () => {
+    return (
+        <div className="circle-image-container">
+            <img className="circle-image" src={CircleImage} alt="Circle" />
+        </div>
+    );
+};
+
+export default Testimonial;
