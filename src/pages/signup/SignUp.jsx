8b2e687c385a380b5cc1b03801efacb1182ea06d
Siyabonga Hadebe <92498693+Psyfo-C-YA@users.noreply.github.com>
2024-03-05 13:47:29 +0200

    Revert "Updating my branch (#16)"
    This reverts commit 3ebd9afa04e458cf79c6e9def9df992b74ff0871.

--- src/pages/signup/SignUp.jsx
+++ src/pages/signup/SignUp.jsx
@@ -1,6 +1,8 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
+import { NavLink } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import "./SignUp.css";
 
-import "./SignUp.css"
 const SignUp = () => {
   const [formData, setFormData] = useState({
     name: "",
@@ -12,6 +14,8 @@ const SignUp = () => {
   });
 
   const [errors, setErrors] = useState({});
+
+  const navigate = useNavigate();
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -25,9 +29,9 @@ const SignUp = () => {
     e.preventDefault();
     const errors = validateForm(formData);
     if (Object.keys(errors).length === 0) {
-
       console.log("Form submitted:", formData);
       setErrors(errors);
+      navigate("/");
     } else {
       setErrors(errors);
     }
@@ -52,120 +56,114 @@ const SignUp = () => {
   };
   return (
     <div className="signup-container">
-    <div className="signup-content">
-      <h1>Welcome to Our Community</h1>
-      <p>
-        Your one-stop solution for home perfection. From plumbing and
-        electrical repairs to pool maintenance and gardening expertise, Our
-        team of skilled professionals is dedicated to delivering unparalleled
-        craftsmanship, reliability, and efficiency in every repair and
-        enhancement, bringing a touch of expertise to every corner of your
-        homes
-      </p>
+      <div className="signup-content">
+        <h1>Welcome to Our Community</h1>
+        <p>
+          Your one-stop solution for home perfection. From plumbing and
+          electrical repairs to pool maintenance and gardening expertise, Our
+          team of skilled professionals is dedicated to delivering unparalleled
+          craftsmanship, reliability, and efficiency in every repair and
+          enhancement, bringing a touch of expertise to every corner of your
+          homes
+        </p>
 
-      <br />
-      <div className="image-stack">
-        <img
-          src="src\assets\register page pic1.png"
-          alt="Community Member 3"
-        />
+        <br />
+        <div className="image-stack">
+          <img
+            src="src\assets\register page pic1.png"
+            alt="Community Member 3"
+          />
 
-        <p>1k plus people have joined our platform now it's your turn!</p>
+          <p>1k plus people have joined our platform now it's your turn!</p>
+        </div>
+      </div>
+
+      <div className="signup-form">
+        <h2>Sign up</h2>
+        <form onSubmit={handleSubmit}>
+          <div className="formFiled">
+            <label>Name </label>
+            <input
+              type="text"
+              name="name"
+              value={formData.name}
+              onChange={handleChange}
+            />{" "}
+            {errors.name && (
+              <span className="error-message">{errors.name}</span>
+            )}
+          </div>
+
+          <div className="formFiled">
+            <label>Surname </label>
+            <input
+              type="text"
+              name="surname"
+              value={formData.surname}
+              onChange={handleChange}
+            />{" "}
+            {errors.surname && (
+              <span className="error-message">{errors.surname}</span>
+            )}
+          </div>
+          <div className="formFiled">
+            <label>Email </label>
+            <input
+              type="email"
+              name="email"
+              value={formData.email}
+              onChange={handleChange}
+            />{" "}
+            {errors.email && (
+              <span className="error-message">{errors.email}</span>
+            )}
+          </div>
+
+          <div className="formFiled">
+            <label>Username </label>
+            <input
+              type="text"
+              name="username"
+              value={formData.username}
+              onChange={handleChange}
+            />{" "}
+            {errors.username && (
+              <span className="error-message">{errors.username}</span>
+            )}
+          </div>
+          <div className="formFiled">
+            {" "}
+            <label>Password </label>
+            <input
+              type="password"
+              name="password"
+              value={formData.password}
+              onChange={handleChange}
+            />{" "}
+            {errors.password && (
+              <span className="error-message">{errors.password}</span>
+            )}
+          </div>
+
+          <div className="formFiled">
+            <label>Confirm Password </label>
+            <input
+              type="password"
+              name="confirmPassword"
+              value={formData.confirmPassword}
+              onChange={handleChange}
+            />{" "}
+            {errors.confirmPassword && (
+              <span className="error-message">{errors.confirmPassword}</span>
+            )}
+          </div>
+          <br />
+
+          <button type="submit">Submit</button>
+        </form>
       </div>
     </div>
+  );
+};
 
-    <div className="signup-form">
-      <h2>Sign up</h2>
-      <form onSubmit={handleSubmit}>
-        <div className="formFiled">
-          <label>Name  </label>
-          <input
-            type="text"
-            name="name"
-            value={formData.name}
-            onChange={handleChange}
-             
-          />{" "}
-          {errors.name && (
-            <span className="error-message">{errors.name}</span>
-          )}
-        </div>
-
-        <div className="formFiled">
-          <label>Surname </label>
-          <input
-            type="text"
-            name="surname"
-            value={formData.surname}
-            onChange={handleChange}
-             
-          />{" "}
-          {errors.surname && (
-            <span className="error-message">{errors.surname}</span>
-          )}
-        </div>
-        <div className="formFiled">
-          <label>Email </label>
-          <input
-            type="email"
-            name="email"
-            value={formData.email}
-            onChange={handleChange}
-             
-          />{" "}
-          {errors.email && (
-            <span className="error-message">{errors.email}</span>
-          )}
-        </div>
-
-        <div className="formFiled">
-          <label>Username </label>
-          <input
-            type="text"
-            name="username"
-            value={formData.username}
-            onChange={handleChange}
-             
-          />{" "}
-          {errors.username && (
-            <span className="error-message">{errors.username}</span>
-          )}
-        </div>
-        <div className="formFiled">
-          {" "}
-          <label>Password </label>
-          <input
-            type="password"
-            name="password"
-            value={formData.password}
-            onChange={handleChange}
-             
-          />{" "}
-          {errors.password && (
-            <span className="error-message">{errors.password}</span>
-          )}
-        </div>
-
-        <div className="formFiled">
-          <label>Confirm Password  </label>
-          <input
-            type="password"
-            name="confirmPassword"
-            value={formData.confirmPassword}
-            onChange={handleChange}
-             
-          />{" "}
-          {errors.confirmPassword && (
-            <span className="error-message">{errors.confirmPassword}</span>
-          )}
-        </div>
-        <br />
-
-        <button type="submit">Submit</button>
-      </form>
-    </div>
-  </div>
-  )
-}
-
-export default SignUp
+export default SignUp;
