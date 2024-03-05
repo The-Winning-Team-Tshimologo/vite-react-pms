7987c3d6d27c0407bca184f98e5f4c959a2f9dc8
Ismael Mours <83817080+IsmaelMours@users.noreply.github.com>
2024-03-05 13:31:22 +0200

    Add files via upload
    i added all the features for landing page

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,37 +1,18 @@
-import React from "react";
-import "./Header.css";
-import { GiHamburgerMenu } from "react-icons/gi";
-import { useAuth } from "@/security/auth/AuthContext";
+import './Header.css'
 
-const Header = () => {
-  const { sidebarCollapsed, setSidebarCollapsed } = useAuth();
-
-  const handleSideBarCollapse = () => {
-    setSidebarCollapsed(!sidebarCollapsed);
-  };
+function Header() {
+  
 
   return (
-    <div className="header__container">
-      <GiHamburgerMenu onClick={handleSideBarCollapse} />
-
-      <div className="header__user-details">
-        <img
-          src="https://media.istockphoto.com/id/155596999/photo/young-south-african-girl-in-classroom.jpg?s=2048x2048&w=is&k=20&c=UQEeHJug5CdnJK50y61bqSaOG29unDz9hwkdV0U6m88="
-          alt=""
-        />
-        <p>Sbusiso Mabaso</p>
-        {/* {user && (
-          <>
-            <img
-              src="https://media.istockphoto.com/id/155596999/photo/young-south-african-girl-in-classroom.jpg?s=2048x2048&w=is&k=20&c=UQEeHJug5CdnJK50y61bqSaOG29unDz9hwkdV0U6m88="
-              alt="profile picture"
-            />
-            <p>{user.firstname} {user.lastname}</p>
-          </>
-        )} */}
+    <>
+     <div id="header" className="header">
+        <span className="flex-row">
+     </span>
+        <span className="flex-row">
+        <button className="header__btn">Join as Pro</button>
+        </span>
       </div>
-    </div>
-  );
-};
+</>
+  )}
+export default Header
 
-export default Header;
