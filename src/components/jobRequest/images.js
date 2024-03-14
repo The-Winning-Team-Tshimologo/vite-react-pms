5fc09759e50ea8ad3cfaead438eb689e968135fc
ismaelmours <monnapuleismaelmours@gmail.com>
2024-03-14 15:31:53 +0200

    revert

---
+++ src/components/jobRequest/images.js
@@ -0,0 +1,6 @@
+export const johnSmithImage = require('../../assets/Ellipse24.png');
+export const emilyJohnsonImage = require('../../assets/Ellipse23.png');
+export const michaelWilliamsImage = require('../../assets/Ellipse25.png');
+export const sarahBrownImage = require('../../assets/pro.png');
+export const davidDavisImage = require('../../assets/Ellipse24.png');
+export const jessicaGarciaImage = require('../../assets/Ellipse25.png');
