f3a54f36cc4e0934b7ff40b30f3e68b5c7db74c7
Psyfo-C-YA <psyfohadebe@gmail.com>
2024-05-10 16:37:54 +0200

    loader to signup for customer and show password added and user friendly ui elements

--- src/pages/customer/signup/SignUp.jsx
+++ src/pages/customer/signup/SignUp.jsx
@@ -1,13 +1,18 @@
-/** @format */
-
 import React, { useState } from "react";
 import { NavLink, useNavigate } from "react-router-dom";
 import { useAuth } from "@/security/auth/AuthContext";
 import "./SignUp.css";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
+import { helix } from "ldrs";
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
+import { AlertCircle } from "lucide-react";
 
 const SignUp = () => {
   const { register } = useAuth(); // Accessing register function from AuthProvider
   const navigate = useNavigate();
+
+  const [isLoading, setLoading] = useState(false);
+  const [failed, setFailed] = useState(false);
 
   const [formData, setFormData] = useState({
     name: "",
@@ -25,21 +30,18 @@ const SignUp = () => {
   });
 
   const [errors, setErrors] = useState({});
+  const [showPassword, setShowPassword] = useState(false);
+  const [showConfirmPassword, setShowConfirmPassword] = useState(false);
 
   const handleChange = (e) => {
     const { name, value } = e.target;
-    if (name.startsWith("address.")) {
-      // If the field is part of the address object
-      const addressField = name.split(".")[1];
+    if (name.includes("address.")) {
+      const fieldName = name.split(".")[1];
       setFormData((prevFormData) => ({
         ...prevFormData,
-        address: {
-          ...prevFormData.address,
-          [addressField]: value,
-        },
+        address: { ...prevFormData.address, [fieldName]: value },
       }));
     } else {
-      // If the field is not part of the address object
       setFormData((prevFormData) => ({
         ...prevFormData,
         [name]: value,
@@ -47,59 +49,64 @@ const SignUp = () => {
     }
   };
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    const errors = validateForm(formData);
-    if (Object.keys(errors).length === 0) {
-      try {
-        // Check if address data is provided
-        if (
-          !formData.address ||
-          !formData.address.streetName ||
-          !formData.address.city ||
-          !formData.address.province ||
-          !formData.address.postalCode
-        ) {
-          throw new Error("Address data is incomplete");
-        }
-
-        const response = await register(formData);
-        if (response) {
-          // Registration successful, redirect or perform any other actions
-          navigate("/signin");
-        } else {
-          // Registration failed, errors will be set in the state by the register function
-          setErrors(`Registration failed: ${error}`);
-        }
-      } catch (error) {
-        console.error("Registration failed:", error);
-        setErrors(`Registration failed: ${error}`);
-      }
-    } else {
-      setErrors(errors);
-    }
-  };
-
   const validateForm = (data) => {
-    const errors = {};
-
-    for (const key in data) {
-      if (!data[key]) {
-        errors[key] = `${
+    const newErrors = {};
+    Object.keys(data).forEach((key) => {
+      if (typeof data[key] === "object") {
+        // Check if any field in the address is empty
+        const addressFields = Object.values(data[key]);
+        if (addressFields.some((field) => field === "")) {
+          newErrors.address = "All address fields are required";
+        }
+      } else if (!data[key] && key !== "confirmPassword") {
+        newErrors[key] = `${
           key.charAt(0).toUpperCase() + key.slice(1)
-        } is required `;
+        } is required`;
       }
-    }
+    });
 
     if (data.password !== data.confirmPassword) {
-      errors.confirmPassword = "Passwords do not match";
+      newErrors.confirmPassword = "Passwords do not match";
     }
 
-    return errors;
+    return newErrors;
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const newErrors = validateForm(formData);
+    setErrors(newErrors);
+
+    if (Object.keys(newErrors).length === 0) {
+      setFailed(false);
+      setLoading(true); // Start loading
+      helix.register();
+      setTimeout(async () => {
+        try {
+          const response = await register(formData);
+          if (response) {
+            navigate("/signin");
+          } else {
+            throw new Error("Registration failed");
+          }
+        } catch (error) {
+          console.error("Registration failed:", error);
+          setErrors({ formError: error.message });
+        }
+      }, 3000);
+    }
   };
 
   return (
     <div className="signup-container">
+      {isLoading && (
+        <>
+          {/* <l-helix size="150" speed="1.5" color="black"></l-helix> */}
+          <div className="loading-overlay">
+            <l-helix size="150" speed="1.5" color="black"></l-helix>
+          </div>
+        </>
+      )}
       <div className="signup-content">
         <h1>Welcome to Our Community</h1>
         <p>
@@ -125,72 +132,86 @@ const SignUp = () => {
         <h2>Sign up</h2>
         <form onSubmit={handleSubmit}>
           <div className="formFiled">
-            <label>Name </label>
+            <label>
+              Name{" "}
+              {errors.name && (
+                <span className="error-message">{errors.name}</span>
+              )}
+            </label>
             <input
               type="text"
               name="name"
               value={formData.name}
               onChange={handleChange}
             />
-            {errors.name && (
-              <span className="error-message">{errors.name}</span>
-            )}
           </div>
 
           <div className="formFiled">
-            <label>Surname </label>
+            <label>
+              Surname{" "}
+              {errors.surname && (
+                <span className="error-message">{errors.surname}</span>
+              )}
+            </label>
             <input
               type="text"
               name="surname"
               value={formData.surname}
               onChange={handleChange}
             />
-            {errors.surname && (
-              <span className="error-message">{errors.surname}</span>
-            )}
           </div>
 
           <div className="formFiled">
-            <label>Email </label>
+            <label>
+              Email{" "}
+              {errors.email && (
+                <span className="error-message">{errors.email}</span>
+              )}
+            </label>
             <input
               type="email"
               name="email"
               value={formData.email}
               onChange={handleChange}
             />
-            {errors.email && (
-              <span className="error-message">{errors.email}</span>
-            )}
           </div>
 
           <div className="formFiled">
-            <label>Username </label>
+            <label>
+              Username{" "}
+              {errors.user_name && (
+                <span className="error-message">{errors.user_name}</span>
+              )}
+            </label>
             <input
               type="text"
               name="user_name"
               value={formData.user_name}
               onChange={handleChange}
             />
-            {errors.user_name && (
-              <span className="error-message">{errors.user_name}</span>
-            )}
           </div>
 
           <div className="formFiled">
-            <label>Address </label>
-            <div className="flex flex-col signup-address__container">
+            <label className="flex gap-1">
+              Address{" "}
+              {errors.address && (
+                <div className="error-message">{errors.address}</div>
+              )}
+            </label>
+
+            <div className="signup-address__container">
               <div className="">
                 <input
                   className=""
                   type="text"
-                  placeholder="Street Name"
+                  placeholder={"Street Name"}
                   name="address.streetName"
                   value={formData.address.streetName}
                   onChange={handleChange}
                 />
-                {errors.streetName && (
+                {/* {errors.streetName && (
                   <span className="error-message">{errors.streetName}</span>
-                )}
+                )} */}
               </div>
 
               <div className="">
@@ -202,9 +223,9 @@ const SignUp = () => {
                   value={formData.address.city}
                   onChange={handleChange}
                 />
-                {errors.city && (
+                {/* {errors.city && (
                   <span className="error-message">{errors.city}</span>
-                )}
+                )} */}
               </div>
 
               <div className="">
@@ -216,9 +237,9 @@ const SignUp = () => {
                   value={formData.address.province}
                   onChange={handleChange}
                 />
-                {errors.province && (
+                {/* {errors.province && (
                   <span className="error-message">{errors.province}</span>
-                )}
+                )} */}
               </div>
 
               <input
@@ -229,40 +250,68 @@ const SignUp = () => {
                 value={formData.address.postalCode}
                 onChange={handleChange}
               />
-              {errors.postalCode && (
+              {/* {errors.postalCode && (
                 <span className="error-message">{errors.postalCode}</span>
-              )}
+              )} */}
             </div>
           </div>
 
           <div className="formFiled">
-            <label>Password </label>
+            <label>
+              Password
+              {errors.password && (
+                <span className="error-message"> {errors.password}</span>
+              )}
+            </label>
             <input
-              type="password"
+              type={showPassword ? "text" : "password"}
               name="password"
               value={formData.password}
               onChange={handleChange}
             />
-            {errors.password && (
-              <span className="error-message">{errors.password}</span>
-            )}
+            <span
+              type="button"
+              onClick={() => setShowPassword(!showPassword)}
+              className="icon-button"
+            >
+              {showPassword ? <FaEyeSlash /> : <FaEye />}
+            </span>
           </div>
 
           <div className="formFiled">
-            <label>Confirm Password </label>
+            <label>
+              Confirm Password
+              {errors.confirmPassword && (
+                <span className="error-message">
+                  {"  "}
+                  {errors.confirmPassword}
+                </span>
+              )}
+            </label>
             <input
-              type="password"
+              type={showConfirmPassword ? "text" : "password"}
               name="confirmPassword"
               value={formData.confirmPassword}
               onChange={handleChange}
             />
-            {errors.confirmPassword && (
-              <span className="error-message">{errors.confirmPassword}</span>
-            )}
+            <span
+              type="button"
+              onClick={() => setShowConfirmPassword(!showConfirmPassword)}
+              className="icon-button"
+            >
+              {showConfirmPassword ? <FaEyeSlash /> : <FaEye />}
+            </span>
           </div>
-         
-          <div className="mt-5"><p>Have an account already? <NavLink to={"/signin"} className={" text-blue-500"}>Signin</NavLink></p></div>
-<br />
+
+          <div className="mt-5">
+            <p>
+              Have an account already?{" "}
+              <NavLink to={"/signin"} className={" text-blue-500"}>
+                Signin
+              </NavLink>
+            </p>
+          </div>
+          <br />
           <button type="submit">Submit</button>
         </form>
       </div>
