cbf259c4e6491657257292d6baaa6c431183a942
Psyfo-C-YA <psyfohadebe@gmail.com>
2024-05-10 20:00:04 +0200

    Empty commit message

--- src/components/JobRequestItem/JobRequestItem.jsx
+++ src/components/JobRequestItem/JobRequestItem.jsx
@@ -1,8 +1,7 @@
-/** @format */
 
 import React from "react";
 import { MdOutlinePinDrop, MdQuestionMark } from "react-icons/md";
-import ServiceRequest from "@/components/serviceRequest/ServiceRequest";
+import ServiceRequest from "@/components/ServiceRequest/ServiceRequest";
 import axios from "axios";
 import { useEffect } from "react";
 
